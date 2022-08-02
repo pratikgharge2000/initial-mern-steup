e4ad1ebf7b3bc004089f78efc9b0a04c7c867be8
Pratik <pratik.gharge@31gmail.com>
2022-08-02 18:14:45 +0530

    add navbar

--- initial code/client/src/App.js
+++ initial code/client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>HELLO FROm PRATIK Updated</h1>
+       <Navbar />
       </header>
     </div>
   );
